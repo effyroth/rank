e693b451841a4e5ec767b1ce4a4210b15f5c3596
effyroth <effyroth@gmail.com>
2014-08-05 14:52:32 +0800

    add times

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ a rank demo
 eg:
 GET POST PUT 
 /rank?appname=test&uid=1000&score=100
+
+GET
+/top?appname=test&uid=1000&score=100
+
+GET PUT
+/times?appname=test&uid=1000
--- main.py
+++ main.py
@@ -30,7 +30,55 @@ class TopHandler(tornado.web.RequestHandler):
         r = Redis(connection_pool=CONNECTION_POOL)
         result = {}
 
-        result["top"] = yield gen.Task(r.zrevrange, "rank:" + appname, 0, 10, "withscore")
+        result["top"] = yield gen.Task(r.zrevrange, "rank:%s" % appname, 0, 10, "withscore")
+        print result
+        self.finish(json.dumps(result))
+
+class TimesHandler(tornado.web.RequestHandler):
+
+    maxtimes = 5
+
+    oncewaittime = 10 * 60
+
+    @tornado.web.asynchronous
+    @gen.engine
+    def get(self):
+        appname = self.get_argument("appname", "unknow")
+        uid = self.get_argument("uid", 0)
+        r = Redis(connection_pool=CONNECTION_POOL)
+        result = {}
+
+        ttl = yield gen.Task(r.ttl, "times:%s:%s" % (appname, uid))
+        if ttl > 0:
+            result["times"] = self.maxtimes - (ttl / self.oncewaittime + 1)
+            result["wait"] = ttl % self.oncewaittime
+            pass
+        else :
+            result["times"] = self.maxtimes
+            result["wait"] = 0
+        print result
+        self.finish(json.dumps(result))
+
+    @tornado.web.asynchronous
+    @gen.engine
+    def put(self):
+        appname = self.get_argument("appname", "unknow")
+        uid = self.get_argument("uid", 0)
+        r = Redis(connection_pool=CONNECTION_POOL)
+        result = {}
+
+        if not (yield gen.Task(r.exists, "times:%s:%s" % (appname, uid))):
+            yield gen.Task(r.set, "times:%s:%s" % (appname, uid), 0)
+
+        ttl = yield gen.Task(r.ttl, "times:%s:%s" % (appname, uid))
+        ttl = ttl or 0
+        if ttl > 4 * self.oncewaittime:
+            result["code"] = -1
+            result["msg"] = "failed"
+        else :
+            code = yield gen.Task(r.expire, "times:%s:%s" % (appname, uid), ttl + self.oncewaittime)
+            result["code"] = 0
+            result["msg"] = "ok"
         print result
         self.finish(json.dumps(result))
 
@@ -44,16 +92,14 @@ class RankHandler(tornado.web.RequestHandler):
         r = Redis(connection_pool=CONNECTION_POOL)
         result = {}
 
-        result["top"] = yield gen.Task(r.zrevrange, "rank:" + appname, 0, 10, "withscore")
+        result["top"] = yield gen.Task(r.zrevrange, "rank:%s" % appname, 0, 10, "withscore")
         result["user"] = {}
-        print appname
-        rank = (yield gen.Task(r.zrevrank, "rank:" + appname, uid))
+        rank = (yield gen.Task(r.zrevrank, "rank:%s" % appname, uid))
         if rank != None:
             ++rank
             pass
-        print "rank:", rank, appname, uid
         result["user"]["rank"] = rank
-        result["user"]["score"] = yield gen.Task(r.zscore, "rank:" + appname, uid)
+        result["user"]["score"] = yield gen.Task(r.zscore, "rank:%s" % appname, uid)
         print result
         self.finish(json.dumps(result))
 
@@ -64,9 +110,8 @@ class RankHandler(tornado.web.RequestHandler):
         appname = self.get_argument("appname", "unknow")
         uid = self.get_argument("uid", 0)
         score = int(self.get_argument("score", 0))
-        print appname, uid, score
         r = Redis(connection_pool=CONNECTION_POOL)
-        ok = yield gen.Task(r.zadd, "rank:" + appname, score, uid)
+        ok = yield gen.Task(r.zadd, "rank:%s" % appname, score, uid)
         self.finish("post:" + str(ok))
 
     @tornado.web.asynchronous
@@ -77,13 +122,15 @@ class RankHandler(tornado.web.RequestHandler):
         score = int(self.get_argument("score", 0))
 
         r = Redis(connection_pool=CONNECTION_POOL)
-        ok = yield gen.Task(r.zadd, "rank:" + appname, score, uid)
+        ok = yield gen.Task(r.zadd, "rank:%s" % appname, score, uid)
         self.finish("post:" + str(ok))
 
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/rank", RankHandler),
     (r"/top", TopHandler),
+    (r"/times", TimesHandler),
+
 ])
 
 
